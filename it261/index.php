e1ccafaf1603c11d463f46d25e6ceb6b9744587c
kingcharlotte <130702963+kingcharlotte@users.noreply.github.com>
2024-02-26 18:35:39 -0800

    Add files via upload

---
+++ it261/index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyPortal page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+
+</head>
+
+<body>
+<h1><a href="index.php">Charlotte's Portal Page</a></h1>
+<h2>The navigation below represents our BIG ASSIGNMENTS</h2>
+    <div id="wrapper">
+<nav>
+<ul>
+<li><a href="website/daily.php">Switch</a></li>
+<li><a href="website/troubleshoot.php">TroubleShoot</a></li>
+<li><a href="weeks/week5/calculator.php">Calculator</a></li>
+<li><a href="">Email</a></li>
+<li><a href="">Database</a></li>
+<li><a href="website/gallery.php">Gallery</a></li>
+</ul>
+
+</nav>
+
+
+<main>
+
+<h2>Hello, My Name is Charlotte King</h2>
+<img class="right" src="images/char.jpeg" alt="Char">
+
+<p> 
+    Nice to meet you! In school I study computer science, math, and physics. 
+    Out of school I enjoy shooting with various cameras (film & digital), creating art, reading, and biking.
+    I am interested in classic literature, philosophy, and music. 
+    Some of my favorite generes of music is 80s, classic rock, 90s, indie/alternative, hiphop & rnb.
+    An academic goal I have is to finish my bachelors in computer science.
+    Thanks for visiting my website!
+</p>
+
+<h2>Localhost</h2>
+<img src="images/charMamp.jpeg" alt="charMamp">
+
+<img src="images/error_msg.jpeg" alt="ErrorMsg">
+
+
+
+</main>
+
+<aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ol>
+<li><a href="weeks/week2/var.php">var.php</a></li>
+<li><a href="weeks/week2/var2.php">var2.php</a></li>
+<li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
+<li><a href="weeks/week2/currency.php">currency.php</a></li>
+<li><a href="weeks/week2/heredoc.php">heredoc.php</a></li>
+</ol>
+
+<h3>Week 3</h3>
+<ol>
+<li><a href="weeks/week3/if.php">if.php</a></li>
+<li><a href="weeks/week3/date.php">date.php</a></li>
+<li><a href="weeks/week3/for-each.php">for-each.php</a></li>
+<li><a href="weeks/week3/for-loop.php">for-loop.php</a></li>
+<li><a href="weeks/week3/switch.php">switch.php</a></li>
+<li><a href="weeks/week3/index.php">index.php</a></li>
+</ol>
+<h3>Week 4</h3>
+<ol>
+<li><a href="weeks/week4/form-get.php">form-get.php</a></li>
+<li><a href="weeks/week4/form1.php">form1.php</a></li>
+<li><a href="weeks/week4/form2.php">form2.php</a></li>
+<li><a href="weeks/week4/form3.php">form3.php</a></li>
+<li><a href="weeks/week4/arithmetic-form.php">arithmetic-form.php</a></li>
+<li><a href="weeks/week4/celcius.php">celcius.php</a></li>
+</ol>
+<h3>Week 5</h3>
+<ol>
+<li><a href="weeks/week5/currency1.php">currency1.php</a></li>
+<li><a href="weeks/week5/currency2.php">currency2.php</a></li>
+<li><a href="weeks/week5/currency3.php">currency3.php</a></li>
+<li><a href="weeks/week5/currency4.php">currency4.php</a></li>
+<li><a href="weeks/week5/calculator.php">calculator.php</a></li>
+<li><a href="weeks/week5/null.php">null.php</a></li>
+</ol>
+
+<h3>Week 6</h3>
+<ol>
+<li><a href="weeks/week6/form.php">form.php</a></li>
+<li><a href="weeks/week6/form2.php">form2.php</a></li>
+<li><a href="weeks/week6/functions.php">functions.php</a></li>
+</ol>
+
+<h3>Week 7</h3>
+<ol>
+<li><a href="weeks/week7/emailable.php">emailable.php</a></li>
+<li><a href="weeks/week7/form3.php">form3.php</a></li>
+<li><a href="weeks/week7/strings.php">strings.php</a></li>
+<li><a href="weeks/week7/pictures.php">pictures.php</a></li>
+<li><a href="weeks/week7/picture2.php">picture2.php</a></li>
+<li><a href="weeks/week7/rand.php">rand.php</a></li>
+</ol>
+
+<h3>Week 8</h3>
+<h3>Final Assignment</h3>
+<ol>
+<li><a href="weeks/week8/people.php">people.php</a></li>
+
+</ol>
+
+
+</aside>
+
+</div>
+
+<footer>
+<ul>
+<li>Copyright &copy;
+    2024</li>
+<li>All Rights Reserved</li>
+<li><a href="index.php">Web Design by Charlotte King</a></li>
+<li><a id="html-checker" href="#">HTML Validation</a></li>
+<li><a id="css-checker" href="#">CSS Validation</a></li>
+</ul>
+    
+<script>
+        document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+        document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+</script>
+    
+</footer>
+
+
+
+</body>
+</html>
---
+++ it261/test.php
@@ -0,0 +1,9 @@
+<?php
+$vegetables[]= 'spinach';
+$vegetables[]= 'broccoli';
+$vegetables[]= 'asparagus';
+$vegetables[]= 'kale';
+echo $vegetables;
+
+
+
